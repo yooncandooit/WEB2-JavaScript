0ea40c4649bd7753773e33b33bc401fb7fa07807
yooncandooit <161998554+yooncandooit@users.noreply.github.com>
2024-04-09 02:34:57 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,12 @@
+function nightDayHandler(self) {
+    var target = document.querySelector('body');
+    if (self.value === 'night') {
+        target.style.backgroundColor = 'black';
+        target.style.color = 'white';
+        self.value = 'day';
+    } else {
+        target.style.backgroundColor = 'white';
+        target.style.color = 'black';
+        self.value = 'night';
+    }
+}
